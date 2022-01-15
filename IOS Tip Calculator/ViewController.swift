74160e992009602dbe6390749f01b351fbfe4b71
ckypros <ckypros@gmail.com>
2022-01-14 23:37:55 -0800

    Initial Commit

---
+++ IOS Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Tip Calculator
+//
+//  Created by Charles Kypros on 1/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
